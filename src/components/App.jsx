041b3a795b979c1806bf6dcc026b87684bf6b6b8
DariaGood <102043020+DariaGood@users.noreply.github.com>
2023-06-13 16:06:24 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
-import { useSelector } from 'react-redux';
 import { Container } from './container/Container';
 import { H1, H2 } from './titles/Title';
-import { P } from './Style/Element.styled';
 import { ContactForm } from './contactForm/ContactForm';
 import { ContactList } from './contactList/ContactList';
 import { Filter } from './filter/Filter';

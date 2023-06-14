765cd279bd448a9e27cccad2a8c806f5090c99ec
DariaGood <102043020+DariaGood@users.noreply.github.com>
2023-06-15 00:39:10 +0300

    Update contatcsApi.js

--- src/redux/contatcsApi.js
+++ src/redux/contatcsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const contactsApi = createApi({
   reducerPath: 'contacts',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://6487b4f7beba62972790eadd.mockapi.io/contacts',
+    baseUrl: 'https://6487b4f7beba62972790eadd.mockapi.io/:endpoint',
   }),
   tagTypes: ['Contact'],
   endpoints: builder => ({
